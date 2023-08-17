6166e3aa34e98e4ec0419e014e148b12bd7764e8
nikmaxon <nikmakson99@gmail.com>
2023-08-17 15:20:17 +0300

    HW 2 advanced задание доработка

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+
+from course.serializers import PaymentSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    user_payment = serializers.IntegerField(source='amount_set.all')
+    user_payment = PaymentSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
