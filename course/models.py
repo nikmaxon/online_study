b6d997a287383427c240598c8ed4e1e41e767ad0
nikmaxon <nikmakson99@gmail.com>
2023-08-17 13:29:40 +0300

    HW 2 в процессе

--- course/models.py
+++ course/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from users.models import NULLABLE
 
 
@@ -21,7 +20,7 @@ class Lesson(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     preview = models.ImageField(**NULLABLE, upload_to='course/', verbose_name='Превью')
-    video_url = models.URLField(**NULLABLE, verbose_name='Ссылка на видео')
+    video_url = models.SlugField(**NULLABLE, verbose_name='Ссылка на видео')
 
     def __str__(self):
         return f'{self.title}'
@@ -31,3 +30,27 @@ class Lesson(models.Model):
         verbose_name_plural = 'Уроки'
 
 
+class Payment(models.Model):
+    CASH = 'CASH'
+    BANK = 'BANK'
+
+    PAYMENT_METHOD_CHOICES = [
+        (CASH, 'Наличные'),
+        (BANK, 'Перевод'),
+    ]
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, **NULLABLE, verbose_name='Пользователь')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, verbose_name='Курс')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, **NULLABLE, verbose_name='Урок')
+
+    amount = models.IntegerField(verbose_name='сумма оплаты')
+    method = models.CharField(max_length=4, choices=PAYMENT_METHOD_CHOICES, **NULLABLE, verbose_name='способ оплаты')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата оплаты')
+
+    def __str__(self):
+        return f'{self.amount}({self.method}) - {self.date}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ('-course', '-lesson', '-amount', '-date', '-method')
--- course/urls.py
+++ course/urls.py
@@ -4,7 +4,7 @@ from course.apps import CourseConfig
 from rest_framework.routers import DefaultRouter
 
 from course.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, PaymentCreateAPIView, PaymentListAPIView
 
 app_name = CourseConfig.name
 
@@ -14,10 +14,16 @@ router.register(r'courses', CourseViewSet, basename='courses')
 
 
 urlpatterns = [
+    # Уроки
     path('lesson/create/', LessonCreateAPIView.as_view(), name='create_lesson'),
     path('lesson/', LessonListAPIView.as_view(), name='list_lesson'),
     path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='get_lesson'),
     path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update_lesson'),
     path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='delete_lesson'),
+
+    # Платежи
+    path('payment/create/', PaymentCreateAPIView.as_view(), name='create_payment'),
+    path('payment/', PaymentListAPIView.as_view(), name='list_payment'),
+
 
 ] + router.urls
