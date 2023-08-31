b78f79a2e2661e9fba097d7ebcde6b51b387f8ad
nikmaxon <nikmakson99@gmail.com>
2023-08-31 12:15:27 +0300

    HW6 celery is ready

--- config/celery.py
+++ config/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # Установка переменной окружения для настроек проекта
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # Создание экземпляра объекта Celery
 app = Celery('config')
